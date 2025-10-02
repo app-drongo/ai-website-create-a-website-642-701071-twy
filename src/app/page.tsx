9e18dcfc56885a38a3d88254ce09329533bf9050
rregti <rachid.regti@gmail.com>
2025-10-02 15:58:05 +0200

    Revert "Add 1 new component(s) to Home page: cta"
    This reverts commit 7a8abe9855e25679e6f9a467c87c3c7c8224bef4.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ export default function HomePage() {
     <>
       <Hero />
       <Features />
-      <Cta />
     </>
   )
 }
-import Cta from '@/components/sections/home/Cta'
--- src/components/sections/home/index.ts
+++ src/components/sections/home/index.ts
@@ -1,3 +1,2 @@
-export { default as Cta } from './Cta'
-export { default as Features } from './Features'
 export { default as Hero } from './Hero'
+export { default as Features } from './Features'
